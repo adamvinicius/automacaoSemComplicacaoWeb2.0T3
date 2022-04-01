fdcddf4d09d1692c21c1b3220856ba9a1f02dac1
adamvinicius <adamvinicius@gmail.com>
2022-03-31 21:15:56 -0300

    aula 06 anotacoes

--- src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
@@ -1,6 +1,9 @@
 package br.com.chronosAcademy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -9,21 +12,28 @@ import static org.junit.Assert.*;
 
 public class TesteWeb {
 
-    @Test
-    public void primeiroTeste(){
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver;
+    ChromeDriver driver;
 
+    @Before
+    public void inicializaTeste(){
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.chronosacademy.com.br");
+    }
+
+    @Test
+    public void primeiroTeste(){
         String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
         assertEquals("Porque Tempo É Conhecimento", titulo);
-
-        driver.quit();
-
-
     }
+
+    @After
+    public void finalizaTeste(){
+        driver.quit();
+    }
+
+
 }
