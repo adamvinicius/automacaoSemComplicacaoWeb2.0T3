4a3971696e45ccaa0fddc8e3a5e30bc5f9ce3943
adamvinicius <adamvinicius@gmail.com>
2022-04-05 21:20:07 -0300

    aula 07 xpath cssselector

--- src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
@@ -24,7 +24,10 @@ public class TesteWeb {
 
     @Test
     public void primeiroTeste(){
-        String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
+        //section[2]/div[3]/div/div/div/div/div[1]/div/h4
+
+        String xpathTitulo = "//section[2]//h4";
+
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
         assertEquals("Porque Tempo É Conhecimento", titulo);
