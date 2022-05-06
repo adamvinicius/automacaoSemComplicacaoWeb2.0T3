f7b58f4ca8dddb9701f934deb059baeb3ffe73dd
adamvinicius <adamvinicius@gmail.com>
2022-05-05 21:12:37 -0300

    aula 14 runner steps

---
+++ src/test/java/br/com/chronosAcademy/runner/RunnerTest.java
@@ -0,0 +1,17 @@
+package br.com.chronosAcademy.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/",
+        tags = "",
+        glue = "src/test/java/br/com/chronosAcademy/steps/",
+        plugin = {"json:target/reports/CucumberReports.json", "pretty"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+)
+public class RunnerTest {
+
+}
---
+++ src/test/java/br/com/chronosAcademy/steps/LoginSteps.java
@@ -0,0 +1,62 @@
+package br.com.chronosAcademy.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LoginSteps {
+    @Dado("que a modal esteja sendo exibida")
+    public void queAModalEstejaSendoExibida() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Quando("for realizado um clique fora da modal")
+    public void forRealizadoUmCliqueForaDaModal() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Entao("a janela modal deve ser fechada")
+    public void aJanelaModalDeveSerFechada() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Quando("for realizado um clique no icone de fechar")
+    public void forRealizadoUmCliqueNoIconeDeFechar() {
+        
+    }
+
+    @Quando("for realizado um clique no link Create New Account")
+    public void forRealizadoUmCliqueNoLinkCreateNewAccount() {
+        
+    }
+
+    @Entao("a pagina Create new Account deve ser exibida")
+    public void aPaginaCreateNewAccountDeveSerExibida() {
+        
+    }
+
+    @Quando("os campos de login forem preenchidos da seguinte forma")
+    public void osCamposDeLoginForemPreenchidosDaSeguinteForma() {
+        
+    }
+
+    @Quando("for realizado um clique no botao sign in")
+    public void forRealizadoUmCliqueNoBotaoSignIn() {
+        
+    }
+
+    @Entao("deve ser possivel logar no sistema")
+    public void deveSerPossivelLogarNoSistema() {
+        
+    }
+
+    @Entao("o sistema deve exibir uma mensagem de erro")
+    public void oSistemaDeveExibirUmaMensagemDeErro() {
+        
+    }
+
+    @Entao("o botao sign in deve permanecer desabilitado")
+    public void oBotaoSignInDevePermanecerDesabilitado() {
+    }
+}
